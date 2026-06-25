32836c65cc3874f1f87c67c035ba86308c154474
RohanSidharta <rohansidharta@gmail.com>
2026-06-25 23:06:46 +0530

    using router conncted all the pages

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,6 +3,8 @@ import{Route} from 'react-router'
 import './App.css'
 import { HomePage } from './pages/HomePage.jsx'
 import { CheckoutPage } from './pages/CheckoutPage.jsx'
+import { OrdersPage } from './pages/OrdersPage.jsx'
+import {Tracking} from './pages/Tracking.jsx'
 
 function App() {
  
@@ -11,6 +13,8 @@ function App() {
     <Routes>
       <Route index element={ <HomePage/>}/>
       <Route path="/checkout" element={<CheckoutPage/>}/>
+      <Route path="/orders" element={<OrdersPage />}></Route>
+      <Route path="/tracking" element={<Tracking />}></Route>
     </Routes>
     
   )
